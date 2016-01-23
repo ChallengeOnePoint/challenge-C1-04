bcd88125b531858127c67854065418ea04a83d55
gregoryRednet <gregory.bernard@rednet.io>
2016-01-23 12:12:51 +0100

    Server templates with express

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ import express from 'express';
 
 const app = express();
 
+app.set('view engine', 'jade');
+
+app.get('/', (req, res) => res.render('index'));
+
 app.get('/api', (req, res) => res.json({message: 'hello world'}));
 
 app.use(express.static('public'));

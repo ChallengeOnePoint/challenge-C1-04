299c94a3de3220da7c7ba0e916946ffc80efcdd3
Jean Dupouy <jean.dupouy@rednet.io>
2016-01-23 12:14:54 +0100

    chore(task): start task

--- server/index.js
+++ server/index.js
@@ -1,19 +1,15 @@
-
 import express from 'express';
 import mongoose from 'mongoose';
 import bluebird from 'bluebird';
 import bodyParser from 'body-parser';
-
-mongoose.Promise = bluebird;
-mongoose.connect('mongodb://localhost/fullstackjs');
-
 import contacts from './controllers/contacts';
 
 const app = express();
 
 app.use(bodyParser.json());
-
 app.use('/api', contacts);
-
 app.use(express.static('public'));
-app.listen(3000, () => console.log('listening port 3000'));
+
+mongoose.Promise = bluebird;
+
+export default app;

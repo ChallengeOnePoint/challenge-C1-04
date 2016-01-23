9528b6bba03278e10960b85c3f98c072f2567e40
Pierre Beaujeu <pierre.beaujeu@rednet.io>
2016-01-23 13:47:41 +0100

    feat(logger): add request logger

--- server/index.js
+++ server/index.js
@@ -7,11 +7,30 @@ import multer from 'multer';
 import contacts from './controllers/contacts';
 
 mongoose.Promise = bluebird;
-//AIzaSyBEwzAmJk2iKrC-kS_xkFfaw7YbFYdeg2A
+
 const app = express();
 
 app.set('view engine', 'jade');
 app.get('/', (req, res) => res.render('index'));
+
+app.use(function requestLogger(req, res, next) {
+  let rEnd = res.end;
+
+  // To track response time
+  req.startTime = Date.now();
+
+  // Proxy the real end function
+  res.end = function(chunk, encoding) {
+    res.end = rEnd;
+    res.end(chunk, encoding);
+
+    let time = Date.now() - req.startTime;
+
+    console.log(`${req.method} ${res.statusCode} - ${time}ms ${req.path}`);
+  };
+
+  next();
+});
 
 app.use(bodyParser.json({
   type: 'application/json',
@@ -25,7 +44,7 @@ app.use(function logErrors(err, req, res, next) {
   console.error(err.message, err.stack);
 
   res.status(500).json({error: 'backend error'});
-  
+
   next();
 });
 
